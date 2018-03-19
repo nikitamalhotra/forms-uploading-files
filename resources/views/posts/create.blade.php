f4c0eca143c5132cbf6f9f521174a00e7232a6e9
Nikita Malhotra <nikita.malhotras@gmail.com>
2018-03-19 16:14:34 +0530

    Forms and Validation (Section 18) Working perfectly fine till lecture 119-124

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <form method="post" action="‌{{ route('posts.store') }}">
+    <form method="post" action='{{ route('posts.store') }}'>
         {{ csrf_field() }}
         <input type="text" name="title" placeholder="Enter Title"><br>
         <textarea name="content" id="content" cols="30" rows="8"></textarea><br>
